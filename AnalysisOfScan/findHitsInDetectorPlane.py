d3c32407da432105ae7ae67ce45f49eac422bff8
Helga Holmestad <helgamargrete@gmail.com>
2017-04-07 13:23:18 +0200

    updating

--- AnalysisOfScan/findHitsInDetectorPlane.py
+++ AnalysisOfScan/findHitsInDetectorPlane.py
@@ -1,3 +1,4 @@
+
 from ROOT import gROOT, TCanvas, TH1D,TH2D,TFile
 gROOT.Reset()
 import matplotlib.colors as colors
@@ -5,6 +6,7 @@ import numpy as np
 import matplotlib
 import math
 import sys
+import re
 
 #this program is to take the output from the ibsimu and filter out the hits  that ended up on the endplate  on the detector.
 
@@ -14,21 +16,23 @@ ztranslation=0.5+0.1/np.sin(40.0/180*np.pi)+0.85*np.cos(np.pi*40.0/180)
 xtranslateion=0.1+0.85*np.sin(40.0*np.pi/180.0);
 
 histo=TH1D("","",100,-2.0,2.0)
-scatter=TH2D("","",10,0,10,100,0.5,1)
+scatter=TH2D("","",100,-1,1,100,-1,1)
 
 #file=open("hitsOnDetectorPlane.txt",'w')
 def findHitsOnDetector(filename):
+    settings=re.search("voltageScan/(.*).txt",filename)
+    print "her"
+    print settings.group(1)
+    output=open("../particlesOnDetector/"+settings.group(1)+".txt",'w')
     hitsOnDetectorPlane=[]
     for line in open(filename,'r'):
         columns=line.split()
-        scatter.Fill(float(columns[3]),float(columns[9]))
         if float(columns[2])<1.2:
             continue
         point=np.matrix([[float(columns[0])-xtranslateion],[float(columns[1])],[float(columns[2])-ztranslation]])
         newPoint=rotation*point
         if  abs(newPoint[0])*abs(newPoint[0])+abs(newPoint[1])*abs(newPoint[1])<0.1*0.1 and abs(newPoint[2])<0.016:
-            #file.write(line)
-            columns=line.split()
+            output.write(line)
             hitsOnDetectorPlane.append(float(columns[3]))
     hits=np.asarray(hitsOnDetectorPlane)
     return len(hitsOnDetectorPlane), np.mean(hits),np.std(hits)
@@ -37,16 +41,17 @@ def findHitsOnDetector(filename):
 #n=writeHitsOnDetector(sys.argv[1])
 
 
-settings=["0","1000","2000","3000","4000","5000"]
-
+settings=["0","1000","2000","3000","4000","5000","6000","7000","8000","9000","10000"]
+settings2=["3000","4000","5000"]
 file=open("statistics.txt","w")
 for i in settings:
     for k in settings:
         for j in settings:
-            hits, mean,standDev=findHitsOnDetector("../voltageScan/D1_0D2_"+i+"E1_-"+k+"E2_-"+str(j)+"_scanning33um.txt")
+            hits, mean,standDev=findHitsOnDetector("../voltageScan/D1_0D2_"+i+"E1_"+k+"E2_"+str(j)+"_scanning33um.txt")
+            #hits, mean,standDev=findHitsOnDetector("../oldData/D1_0D2_3000E1_-4000E2_-"+str(j)+"_scanning81.txt")
             file.write(str(0)+"  "+i+"  "+k+"  "+j+"  "+str(hits)+"  "+str(mean)+"  "+str(standDev)+"\n") 
-            print findHitsOnDetector("../voltageScan/D1_0D2_"+i+"E1_-"+k+"E2_-"+str(j)+"_scanning33um.txt")
-            print i,k,j
+            #print findHitsOnDetector("../voltageScan/D1_0D2_"+i+"E1_-"+k+"E2_-"+str(j)+"_scanning33um.txt")
+            #print i,k,j
             
 #print "hitsOnDetector",0.34*1.4*1.4*n*1.0/(10*10*np.pi)
 file.close()
